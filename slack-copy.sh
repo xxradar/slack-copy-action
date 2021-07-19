c5375bb257058bca0dbabcd565ea066b8c4c24ab
Philippe Bogaerts <philippe.bogaerts@radarhack.com>
2021-07-19 20:28:42 +0200

    Update slack-copy.sh

--- slack-copy.sh
+++ slack-copy.sh
@@ -2,7 +2,7 @@
 echo dadadada >test.txt
 echo "This is the slack token: --" $SLACK_TOKEN "--"
 curl -F file='@'$1  \
-     -F "initial_comment=File upload demo "$1 \
+     -F "initial_comment=File upload name: "$1 \
      -F channels=alerts \
      -H "Authorization: Bearer "$SLACK_TOKEN \
       https://slack.com/api/files.upload
